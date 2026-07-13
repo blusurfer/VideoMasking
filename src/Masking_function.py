c8979b57b2de69eb7b033da9de5981bfe57a7c77
bozunhuang <bozun@outlook.com>
2026-07-14 00:23:56 +0800

    finished image processing functions

--- src/Masking_function.py
+++ src/Masking_function.py
@@ -1,7 +1,8 @@
-def biyilun(food):
-    print("这是一个比翼鸟函数")
-    print("我是一个迪克")
-    return food
+import numpy as np
+import av
+import av.datasets
 
-def huangbozun():
-    return "shabi"
+container = av.open(av.datasets.curated("pexels/time-lapse-video-of-night-sky-857195.mp4"))
+
+frames = [frame for frame in container.decode(video=0)]
+frames[0].to_image().save("/images/first_frame.png")
